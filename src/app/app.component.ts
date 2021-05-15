a67819814a0dd4bdedfdf81eedae2685c76fe4dc
Khaled El Hamzi <contact@elhamzi.me>
2021-05-15 19:09:50 +0200

    Change detection in action with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Tweet } from './types/like';
 
 @Component({
@@ -7,6 +7,7 @@ import { Tweet } from './types/like';
 export class AppComponent {
   title = 'angular-fundamentals';
   courses: number[] = [1, 2];
+  id: number = 1;
 
   tweet: Tweet = {
     body: 'Here is the body of the tweet',
@@ -14,9 +15,21 @@ export class AppComponent {
     likesCount: 10
   }
 
-  lessons = [
-    { id: 1, name: "lesson1" },
-    { id: 2, name: "lesson2" },
-    { id: 3, name: "lesson3" }
+  lessons: {id:number, name:string}[] = [
+    { id: this.id, name: `lesson ${this.id}` },
   ];
+
+  onAdd() {
+    this.id += 1;
+    this.lessons.push({ id: this.id, name: `lesson ${this.id}` });
+  }
+
+  onRemove(lesson: { id: number, name: string }) {
+    let index = this.lessons.indexOf(lesson);
+    this.lessons.splice(index, 1);
+  }
+
+  onChange(lesson: { id: number, name: string }) {
+    lesson.name = `lesson ${lesson.id} UPDATED `
+  }
 }
