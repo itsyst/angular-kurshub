fe1c720a8579a84bd7fd469028f3a775e0b2c307
Khaled El Hamzi <contact@elhamzi.me>
2021-05-18 16:24:31 +0200

    Simulate async validaors

--- src/app/common/validators/username.validators.ts
+++ src/app/common/validators/username.validators.ts
@@ -4,7 +4,12 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null{
     if ((control.value as string).indexOf(' ') >= 0)
       return { cannotContainSpace: true }
-
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl){
+    if (control.value.toLowerCase() === 'khaled')
+      return ({ shouldBeUnique: true });
+    return(null);
   }
 }
