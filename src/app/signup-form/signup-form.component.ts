8166db07ca0beda41773e14c7f67f448187088c4
Khaled El Hamzi <contact@elhamzi.me>
2021-05-18 23:55:42 +0200

    Use nested groups in a form

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,16 +9,18 @@ import { UsernameValidators } from '../common/validators/username.validators';
 })
 export class SignupFormComponent {
   signUp: FormGroup = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace,
-      UsernameValidators.shouldBeUnique
-    ]) ,
-    password: new FormControl('******', [
-      Validators.required,
-      Validators.minLength(6)
-    ])
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace,
+        UsernameValidators.shouldBeUnique
+      ]) ,
+      password: new FormControl('******', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
   });
 
   // login() {
@@ -37,10 +39,10 @@ export class SignupFormComponent {
   }
 
   get username(){
-    return this.signUp?.get('username');
+    return this.signUp?.get('account.username');
   };
 
   get password(){
-  return this.signUp.get('password');
+  return this.signUp.get('account.password');
   };
 }
