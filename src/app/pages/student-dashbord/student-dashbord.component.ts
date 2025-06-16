c780741bf044a58802e835af11cd1c51317bf617
Khaled El Hamzi <contact@elhamzi.me>
2025-06-16 16:42:19 +0200

    feat: add student components.

---
+++ src/app/pages/student-dashbord/student-dashbord.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-dashbord',
+  imports: [],
+  templateUrl: './student-dashbord.component.html',
+})
+export class StudentDashbordComponent {
+
+}
