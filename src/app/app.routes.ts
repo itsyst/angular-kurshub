d349a5ab7173076299d1b927f8b6f62d20814d7b
Khaled El Hamzi <contact@elhamzi.me>
2025-06-16 16:40:34 +0200

    feat: update routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,34 +3,56 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'courses',
-    loadComponent: () => import('./pages/courses/courses.component').then(m => m.CoursesComponent)
+    loadComponent: () =>
+      import('./pages/courses/courses.component').then(
+        (m) => m.CoursesComponent
+      ),
+  },
+  {
+    path: 'courses/:id',
+    loadComponent: () =>
+      import('./pages/course-detail/course-detail.component').then(
+        (m) => m.CourseDetailComponent
+      ),
   },
   {
     path: 'authors',
-    loadComponent: () => import('./pages/authors/authors.component').then(m => m.AuthorsComponent)
+    loadComponent: () =>
+      import('./pages/authors/authors.component').then(
+        (m) => m.AuthorsComponent
+      ),
   },
   {
     path: 'about',
-    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'register',
-    loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
   },
   {
     path: 'contact',
-    loadComponent: () => import('./pages/contact-form/contact-form.component').then(m => m.ContactFormComponent)
+    loadComponent: () =>
+      import('./pages/contact-form/contact-form.component').then(
+        (m) => m.ContactFormComponent
+      ),
   },
   {
     path: '**',
     redirectTo: 'not-found',
-  }
-]
+  },
+];
